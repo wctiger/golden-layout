f30b77a34d55d9cb13b39c3b4a70f1e29aa0bbed
Norbert de Langen <ndelangen@me.com>
2018-04-26 20:21:03 +0200

    FIX react

--- src/js/utils/ReactComponentHandler.js
+++ src/js/utils/ReactComponentHandler.js
@@ -27,17 +27,29 @@ lm.utils.copy(lm.utils.ReactComponentHandler.prototype, {
    * @private
    * @returns {void}
    */
-  _render() {
-    this._reactComponent = ReactDOM.render(
-      this._getReactComponent(),
-      this._container.getElement()[0]
-    );
-    this._originalComponentWillUpdate = this._reactComponent.componentWillUpdate || function() {};
-    this._reactComponent.componentWillUpdate = this._onUpdate.bind(this);
-    if (this._container.getState()) {
-      this._reactComponent.setState(this._container.getState());
-    }
-  },
+	_render: function() {
+		ReactDOM.render( this._getReactComponent(), this._container.getElement()[ 0 ] );
+	},
+
+  /**
+	 * Fired by react when the component is created.
+	 * <p>
+	 * Note: This callback is used instead of the return from `ReactDOM.render` because
+	 *	   of https://github.com/facebook/react/issues/10309.
+	 * </p>
+	 *
+	 * @private
+	 * @arg {React.Ref} component The component instance created by the `ReactDOM.render` call in the `_render` method.
+	 * @returns {void}
+	 */
+	_gotReactComponent: function(component) {
+		this._reactComponent = component;
+		this._originalComponentWillUpdate = this._reactComponent.componentWillUpdate || function() {};
+		this._reactComponent.componentWillUpdate = this._onUpdate.bind( this );
+		if( this._container.getState() ) {
+			this._reactComponent.setState( this._container.getState() );
+		}
+	},
 
   /**
    * Removes the component from the DOM and thus invokes React's unmount lifecycle
@@ -45,11 +57,11 @@ lm.utils.copy(lm.utils.ReactComponentHandler.prototype, {
    * @private
    * @returns {void}
    */
-  _destroy() {
-    ReactDOM.unmountComponentAtNode(this._container.getElement()[0]);
-    this._container.off('open', this._render, this);
-    this._container.off('destroy', this._destroy, this);
-  },
+	_destroy: function() {
+		ReactDOM.unmountComponentAtNode( this._container.getElement()[ 0 ] );
+		this._container.off( 'open', this._render, this );
+		this._container.off( 'destroy', this._destroy, this );
+	},
 
   /**
    * Hooks into React's state management and applies the componentstate
@@ -58,10 +70,10 @@ lm.utils.copy(lm.utils.ReactComponentHandler.prototype, {
    * @private
    * @returns {void}
    */
-  _onUpdate(nextProps, nextState) {
-    this._container.setState(nextState);
-    this._originalComponentWillUpdate.call(this._reactComponent, nextProps, nextState);
-  },
+	_onUpdate: function( nextProps, nextState ) {
+		this._container.setState( nextState );
+		this._originalComponentWillUpdate.call( this._reactComponent, nextProps, nextState );
+	},
 
   /**
    * Retrieves the react class from GoldenLayout's registry
@@ -69,24 +81,23 @@ lm.utils.copy(lm.utils.ReactComponentHandler.prototype, {
    * @private
    * @returns {React.Class}
    */
-  _getReactClass() {
-    const componentName = this._container._config.component;
+	_getReactClass: function() {
+		var componentName = this._container._config.component;
+		var reactClass;
 
-    if (!componentName) {
-      throw new Error('No react component name. type: react-component needs a field `component`');
-    }
+		if( !componentName ) {
+			throw new Error( 'No react component name. type: react-component needs a field `component`' );
+		}
 
-    const reactClass = this._container.layoutManager.getComponent(componentName);
+		reactClass = this._container.layoutManager.getComponent( componentName );
 
-    if (!reactClass) {
-      throw new Error(
-        `React component "${componentName}" not found. ` +
-          `Please register all components with GoldenLayout using \`registerComponent(name, component)\``
-      );
-    }
+		if( !reactClass ) {
+			throw new Error( 'React component "' + componentName + '" not found. ' +
+				'Please register all components with GoldenLayout using `registerComponent(name, component)`' );
+		}
 
-    return reactClass;
-  },
+		return reactClass;
+	},
 
   /**
    * Copies and extends the properties array and returns the React element
@@ -95,11 +106,12 @@ lm.utils.copy(lm.utils.ReactComponentHandler.prototype, {
    * @returns {React.Element}
    */
   _getReactComponent() {
-    const defaultProps = {
-      glEventHub: this._container.layoutManager.eventHub,
-      glContainer: this._container,
-    };
-    const props = $.extend(defaultProps, this._container._config.props);
-    return React.createElement(this._reactClass, props);
+		var defaultProps = {
+			glEventHub: this._container.layoutManager.eventHub,
+			glContainer: this._container,
+			ref: this._gotReactComponent.bind( this )
+		};
+		var props = $.extend( defaultProps, this._container._config.props );
+		return React.createElement( this._reactClass, props );
   },
 });
